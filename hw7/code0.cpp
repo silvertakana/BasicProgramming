db54e19603a7c3dd24f6ca35e8ee12a710481d7b
silver takana <titmitna@gmail.com>
2021-10-24 14:35:51 +1300

    yeah boiiiii

---
+++ hw7/code0.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <array>
+
+const size_t n = 8;
+void backtrack(std::array<size_t, n> board, size_t lineOn = 0)
+{
+	// show board
+	if (lineOn == n)
+	{
+		char black = 178, white = 176;
+		for (size_t j = 0; j < n; j++)
+		{
+			for (size_t i = 0; i < n; i++)
+			{
+				if (board[j] == i)
+					std::cout << "QU";
+				else
+					for (size_t k = 0; k < 2; k++)
+					{
+						std::cout << (((i + j + 1) % 2) == 0 ? white : black);
+					}
+			}
+			std::cout << "\n";
+		}
+		std::cout << "\n";
+		return;
+	}
+
+	for (size_t x = 0; x < n; x++)
+	{
+		size_t checkX = 0, checkY = 0;
+		bool isAvailable = true;
+		for (size_t i = 0; i < lineOn; i++)
+		{
+			if (board[i] != (size_t)-1)
+			{
+				checkX = board[i];
+				checkY = i;
+
+				if (x == checkX || abs((int)(x - checkX)) == abs((int)(lineOn - checkY)))
+				{
+					isAvailable = false;
+					break;
+				}
+			}
+			checkY++;
+			continue;
+		}
+		if (isAvailable)
+		{
+			auto b2 = board;
+			b2[lineOn] = x;
+			backtrack(b2, lineOn + 1);
+		}
+	}
+}
+int main()
+{
+	std::array<size_t, n> board;
+	board.fill(-1);
+
+	backtrack(board);
+}
---
+++ hw7/code1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <chrono>
+
+double pow2A(uint64_t n)
+{
+	return n == 0 ? 1 : (2 * pow2A(n - 1));
+}
+double pow2B(uint64_t n)
+{
+	return n == 0 ? 1 : (pow2A(n - 1) + pow2A(n - 1));
+}
+int main()
+{
+	uint64_t n=100;
+	{
+		auto startTime = std::chrono::high_resolution_clock::now();
+		std::cout << pow2A(n);
+		auto duraion = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime);
+		std::cout << " in " << std::fixed << duraion.count() / 1000000.f << "\n";
+	}
+	{
+		auto startTime = std::chrono::high_resolution_clock::now();
+		std::cout << pow2B(n);
+		auto duraion = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::high_resolution_clock::now() - startTime);
+		std::cout << " in " << std::fixed << duraion.count() / 1000000.f << "\n";
+	}
+}
