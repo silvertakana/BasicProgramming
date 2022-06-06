5d9fd4513d42d90648340e63e7b7a90e6246908f
silver takana <titmitna@gmail.com>
2022-06-06 19:09:57 +1200

    fix code1

--- FinalTest/code1.cpp
+++ FinalTest/code1.cpp
@@ -3,14 +3,42 @@
 #include <vector>
 #include <map>
 
-std::vector<std::string> landMap{
-	"1 1 1 1 0",
-	"1 1 0 1 0",
-	"0 0 0 0 0",
-	"1 1 0 0 0",
-};
-
+template <class T>
+void promt(std::string text, T &input)
+{
+	std::cout << text;
+	std::cin >> input;
+}
 int main()
 {
-
+	size_t arraySize;
+	promt("Enter array size: ", arraySize);
+	std::cout << "enter array elements: " << std::endl;
+	std::vector<int> array(arraySize);
+	for (size_t i = 0; i < arraySize; i++)
+	{
+		int element;
+		promt("", element);
+		array[i] = element;
+	}
+	std::cout << "array: " << std::endl;
+	for (size_t i = 0; i < arraySize; i++)
+	{
+		std::cout << array[i] << " ";
+	}
+	std::cout << "\nresult: " << std::endl;
+	std::map<int, bool> m;
+	for (size_t i = 0; i < arraySize; i++)
+	{
+		if (m.find(array[i]) != m.end())
+			m[array[i]] = true;
+		m[array[i]];
+	}
+	for (auto i : m)
+	{
+		if (i.second == false)
+		{
+			std::cout << i.first << " ";
+		}
+	}
 }
